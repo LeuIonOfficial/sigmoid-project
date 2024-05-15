e0ef1398ac853af1f09bebdb8583b116c37ba80e
Ion Leu <leuionofficial@gmail.com>
2024-05-15 22:55:17 +0300

    Finish JWT configuration

--- backend/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('me/', views.GetUser.as_view(), name='me'),
-    path('register/', views.CreateUser.as_view(), name='register'),
-]
