c344dec0258b95210a2859db9f247ed7f7297399
Ion Leu <leuionofficial@gmail.com>
2024-05-29 18:17:36 +0300

    Small changes

--- backend/user_api/serializers.py
+++ backend/user_api/serializers.py
@@ -10,9 +10,9 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = UserModel
         fields = '__all__'
 
-    def create(self, clean_data):
+    def create(self, validated_data):
         user_obj = UserModel.objects.create_user(
-            email=clean_data['email'], password=clean_data['password'], username=clean_data["username"])
+            email=validated_data['email'], password=validated_data['password'], username=clean_data["username"])
         user_obj.save()
 
         return user_obj
@@ -22,8 +22,7 @@ class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
 
-    @staticmethod
-    def check_user(clean_data):
+    def check_user(self, clean_data):
         user = authenticate(
             username=clean_data['email'], password=clean_data['password'])
         if not user:
